1d4a956e6015e24de78df23049444c73ede992f9
ThetaSigma <gerronimoo@gmail.com>
2023-07-25 07:24:20 +0300

    Добавил RecyclerView для категорий,начал работу на viewpager2 и tablayout

--- app/src/main/java/com/example/cashflow/MainActivity.java
+++ app/src/main/java/com/example/cashflow/MainActivity.java
@@ -1,21 +1,61 @@
 package com.example.cashflow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.example.cashflow.budget.BudgetMain;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager2 viewPager2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] tabNames = new String[]{"Главная","Транзакции","Категории", "Счета"};
+        ViewPager2 viewPager2 = findViewById(R.id.pager);
+        FragmentStateAdapter fragmentStateAdapter = new ViewPagerStateAdapter(this);
+        viewPager2.setAdapter(fragmentStateAdapter);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Главная"));
+        tabLayout.addTab(tabLayout.newTab().setText("Операции"));
+        tabLayout.addTab(tabLayout.newTab().setText("Категории"));
+        tabLayout.addTab(tabLayout.newTab().setText("Счета"));
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        /*TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabNames[position]);
+            }
+        });
+        tabLayoutMediator.attach();*/
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+               tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cashflow/ViewPagerStateAdapter.java
+++ app/src/main/java/com/example/cashflow/ViewPagerStateAdapter.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.example.cashflow.budget.BudgetAccounts;
+import com.example.cashflow.budget.BudgetCategories;
+import com.example.cashflow.budget.BudgetExpenses;
+import com.example.cashflow.budget.BudgetMain;
+
 public class ViewPagerStateAdapter extends FragmentStateAdapter {
     private static final int TAB_ITEM_SIZE = 4;
     public ViewPagerStateAdapter(@NonNull FragmentActivity fragmentActivity) {
@@ -24,7 +29,15 @@ public class ViewPagerStateAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return null;
+        switch (position){
+            case 1:
+                return new BudgetExpenses();
+            case 2:
+                return new BudgetCategories();
+            case 3:
+                return new BudgetAccounts();
+        }
+        return new BudgetMain();
     }
 
     @Override
